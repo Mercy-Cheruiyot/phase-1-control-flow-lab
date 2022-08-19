9922292fbd30f8535caf09ded5d517cc5cc7bd5c
Mercy <mercycheruiyot2013@gmail.com>
2022-08-19 18:25:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
+
+  if (someValue <= 400 ){
+    return "This one is on me!";
+  }
+  else if(someValue>2500){
+   return "No can do.";
+  }
+  else if(someValue > 2000){
+    return "I will gladly take your thirty bucks.";
+   }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){ //using the ternary check
+ return(city==="NYC"? "Ok, sounds good.":"No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case "generous":
+      return "Thank you so much.";
+      break;
+      case "not as generous":
+        return "Thank you.";
+        break;
+        default :
+        return "Bye."
+  }
+  
 }
